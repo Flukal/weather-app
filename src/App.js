e1896708429ee4d633be07aa17f5556ec6f8e234
Flukal <fluviobass@gmail.com>
2019-05-11 11:11:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
-import SearchForm from './components/searchForm';
-import SearchResult from './components/searchResult';
+import Form from './components/Form';
+import Title from './components/Title';
+import Weather from './components/Weather';
+
+const API_KEY = '6e84277ec0dc4270fd29cf64bcae2a55';
 
 class App extends Component {
+  state = {
+    city: undefined,
+    country: undefined,
+    description: undefined,
+    temperature: undefined,
+    humidity: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+    if( city && country ){
+      this.setState({
+        city: data.name,
+        country: data.sys.country,
+        description: data.weather[0].description,
+        temperature: data.main.temp,
+        humidity: data.main.humidity,
+        error: ""
+      })
+    }else{
+      this.setState({
+        city: undefined,
+        country: undefined,
+        description: undefined,
+        temperature: undefined,
+        humidity: undefined,
+        error: "Please insert city and country"
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <SearchForm />
-        <SearchResult />
+        <Title />
+        <Form getWeather={this.getWeather} />
+        <Weather 
+        city={this.state.city}
+        country={this.state.country}
+        description={this.state.description}
+        temperature={this.state.temperature}
+        humidity={this.state.humidity}
+        error={this.state.error}
+        />
       </div>
     );
   }
---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Title = () => (
+
+    <div>
+        Weather App
+    </div>
+)
+
+export default Title;
