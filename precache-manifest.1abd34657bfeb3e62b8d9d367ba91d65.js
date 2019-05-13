9c916c3a64169e7e5d2b3d5528700c6649565136
Flukal <fluviobass@gmail.com>
2019-05-13 13:34:33 +0200

    Updates

---
+++ precache-manifest.1abd34657bfeb3e62b8d9d367ba91d65.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eca77e381dd9f809e503be800b2d860",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "ab76449773f040809588",
+    "url": "/weather-app/static/js/2.61d2c586.chunk.js"
+  },
+  {
+    "revision": "ee61a6f220c88032ccc2",
+    "url": "/weather-app/static/js/main.d8e53bdd.chunk.js"
+  },
+  {
+    "revision": "90e6a6c358468ae00209",
+    "url": "/weather-app/static/js/runtime~main.4606d7f5.js"
+  }
+]);
